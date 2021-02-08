fa06cae0f8ed9b254ee9f3fa219577f2f936539c
Hector <hectormrg0823@gmail.com>
2021-02-08 14:14:10 -0800

    project skeleton

---
+++ src/main/java/com/PPwebapp/PPwebapp/PPwebappApplication.java
@@ -0,0 +1,13 @@
+package com.PPwebapp.PPwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PPwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PPwebapp/PPwebapp/PPwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPwebapp.PPwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
